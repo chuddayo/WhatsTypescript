ef020c506c425a543e18c81341088655ac07662f
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-13 19:05:47 -0400

    tsc acquired

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 // Import stylesheets
 import './style.css';
 
-
 const form: HTMLFormElement = document.querySelector('#defineform');
-
 
 form.onsubmit = () => {
   const formData = new FormData(form);
+  // https://api.dictionaryapi.dev/api/v2/entries/en/hello
 
   console.log(formData);
   const text = formData.get('defineword') as string;
