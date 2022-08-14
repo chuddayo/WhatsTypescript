27230c2ee416f6c53ae8bcaa5c1975d2d4ab6da4
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-08-13 22:09:22 -0400

    lookup your word to json in console

--- index.ts
+++ index.ts
@@ -1,14 +1,51 @@
-// Import stylesheets
-import './style.css';
+// https://api.dictionaryapi.dev/api/v2/entries/en/hello
 
+
+// Import stylesheets
+//import './style.css';
+
+
+const dictionaryURL: string = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const form: HTMLFormElement = document.querySelector('#defineform');
 
 form.onsubmit = () => {
-  const formData = new FormData(form);
-  // https://api.dictionaryapi.dev/api/v2/entries/en/hello
+  const formData = new FormData(form!);
 
   console.log(formData);
   const text = formData.get('defineword') as string;
   console.log(text);
+
+  getDefinition(text);
+
   return false; // prevent reload
 };
+
+async function getDefinition(text: string) {
+  try {
+    const response = await fetch(dictionaryURL + text, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = (await response.json());
+
+    console.log('result is: ', JSON.stringify(result, null, 4));
+
+    return result;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
